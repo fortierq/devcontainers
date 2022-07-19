a5e8b71dd553294ea2d997c848f7a882b9bf5183
Quentin Fortier <qpfortier@gmail.com>
2022-07-19 21:18:48 +0200

    Update

--- make.py
+++ make.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
-import typer
+import json
+from pathlib import Path
 import subprocess
+import typer
+
+from settings import repository_to_devcontainer
 
 make = typer.Typer()
 
-def repository(image, tag):
+def repository(image: str, tag: str) -> str:
     return f"qfortier/{image}:{tag}"
 
 @make.command()
@@ -17,5 +21,15 @@ def push(image: str, tag: str="latest"):
     build(image, tag)
     subprocess.run(["docker", "push", repository(image, tag)])
 
+@make.command()
+def devcontainer(name: str):
+    d = dict()
+    for p in Path(".").glob("**"):
+        if p.is_dir() and p.name == name:
+            for part in p.relative_to(".").parts:
+                if (file := Path(part) / "devcontainer.json").exists():
+                    d |= json.loads(file.read_text())
+            
+    
 if __name__ == "__main__":
     make()
